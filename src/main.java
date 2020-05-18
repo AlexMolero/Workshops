016fac3632fe50df133f53620dce7ea5b263cee5
Alejandro Viana Labà <alejandro.viana@students.salle.url.edu>
2020-05-18 17:45:16 +0200

    View

--- src/main.java
+++ src/main.java
@@ -1,11 +1,82 @@
+import view.ScheduleView;
+
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.net.StandardSocketOptions;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args) {
-        // write your code here
+        //Prepare view
+        final ScheduleView view = new ScheduleView();
+
+        //Show view
+        SwingUtilities.invokeLater(() -> view.setVisible(true));
+
+        /*****************
+         SAMPLE CODE
+         ****************/
+        //Set cells content
+        int[] categories = new int[5];
+        float cost = 0;
+        for(int i = 0; i < 12; i++) {
+            for(int j = 0; j < 5; j++) {
+                if (i % 2 == 0) {
+                    if(j % 2 == 0) {
+                        view.setCellContent(String.format("Row: %d - Col: %d", i, j), 1,
+                                i * j, Color.ORANGE, i, j);
+                    } else {
+                        view.setCellContent(String.format("Row: %d - Col: %d", i, j), 1,
+                                i * j, Color.PINK, i, j);
+                    }
+                    categories[1 - 1]++;
+                } else {
+                    if(j % 2 == 0) {
+                        view.setCellContent(String.format("Row: %d - Col: %d", i, j), 2,
+                                i * j, Color.CYAN, i, j);
+                    } else {
+                        view.setCellContent(String.format("Row: %d - Col: %d", i, j), 2,
+                                i * j, Color.GREEN, i, j);
+                    }
+                    categories[2 - 1]++;
+                }
+                cost += i * j;
+            }
+        }
+
+        //Erase some cells
+        view.resetCellContent(4, 0);
+        categories[1 - 1]--;
+        view.resetCellContent(11, 1);
+        categories[2 - 1]--;
+        cost -= 11.0f;
+        view.resetCellContent(0, 1);
+        categories[1 - 1]--;
+
+        //Set generic information
+        view.setStartDateContent(LocalDateTime.now());
+        view.setFinishDateContent(LocalDateTime.now());
+        view.setDurationContent(Duration.ZERO);
+        view.setSolutionsContent(10);
+
+        //Set time optimization information
+        view.setTotalWorkshopsContent(57);
+        view.setTotalHoursContent(57);
+
+        //Set cost information
+        view.setLimitCostContent(10000);
+        view.setBaseCostContent(cost);
+        view.setDiscountContent(15);
+        view.setFinalCostContent(cost * 0.85f);
+        for(int i = 0; i < categories.length; i++) {
+            view.setCategoryContent(i + 1, categories[i]);
+        }
+
+
         System.out.println("_-_-_- WorkshopScheduler -_-_-_ ");
         Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu(scanner);
